303d51c3ea76d5b2375912e1e1fc498e7982dba7
Akshay5119 <pawarab100@google.com>
2024-11-25 18:21:41 +0530

    new_vpcafter_s3_tf

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
-provider "aws"{
-    region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.30.0.0/16"
+# First VPC
+resource "aws_vpc" "my_vpc_1" {
+  cidr_block = "10.40.0.0/16"
 
+  tags = {
+    Name = "My First VPC"
+  }
+}
+
+# Second VPC
+resource "aws_vpc" "my_vpc_2" {
+  cidr_block = "10.50.0.0/16"
+
+  tags = {
+    Name = "My Second VPC"
+  }
 }
